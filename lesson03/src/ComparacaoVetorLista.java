8547c82d68acc5af3af85763a3099fc563034b8a
Thasyo Peres <thasyotp2002@gmail.com>
2025-03-24 23:12:38 -0300

    comparação de desempenho entre vetor e lista

---
+++ lesson03/src/ComparacaoVetorLista.java
@@ -0,0 +1,54 @@
+import java.util.ArrayList;
+
+public class ComparacaoVetorLista {
+    public static void main(String[] args) {
+        ListaLigada<Integer> lista = new ListaLigada<Integer>();
+        ArrayList<Integer> vetor = new ArrayList<Integer>();
+
+        // Adicionando Elementos
+        int limite = 10000;
+        long tempoInicial = System.currentTimeMillis();
+        long tempoFinal;
+
+        //vetor
+        for (int i = 0; i < limite; i++){
+            vetor.add(i);
+        }
+        tempoFinal = System.currentTimeMillis();
+
+        System.out.print("Tempo de adição de elementos no vetor: ");
+        System.out.println(tempoFinal - tempoInicial);
+
+        //lista
+        tempoInicial = System.currentTimeMillis();
+        for (int i = 0; i < limite; i++){
+            lista.adicionar(i);
+        }
+        tempoFinal = System.currentTimeMillis();
+
+        System.out.print("Tempo de adição de elementos na lista: ");
+        System.out.println(tempoFinal - tempoInicial);
+
+        //Acessando elementos
+
+        //vetor
+        tempoInicial = System.currentTimeMillis();
+        for (int i = 0; i < vetor.size(); i++){
+            vetor.get(i);
+        }
+        tempoFinal = System.currentTimeMillis();
+
+        System.out.print("Tempo de acesso aos elementos no vetor: ");
+        System.out.println(tempoFinal - tempoInicial);
+
+        //lista
+        tempoInicial = System.currentTimeMillis();
+        for (int i = 0; i < lista.getTamanho(); i++){
+            lista.get(i);
+        }
+        tempoFinal = System.currentTimeMillis();
+
+        System.out.print("Tempo de acesso aos elementos na lista: ");
+        System.out.println(tempoFinal - tempoInicial);
+    }
+}
